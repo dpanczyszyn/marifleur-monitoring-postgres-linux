5c9c58a7269fdea9666406eb7e02d1fbe21d5d07
dominikpanczyszyn1@gmail.com <dominikpanczyszyn1@gmail.com>
2024-01-30 19:38:35 +0100

    add getMicroserviceBaseUrl method to NetUtil

--- src/main/java/pl/marifleur/microservice/postgres/linux/util/net/NetUtil.java
+++ src/main/java/pl/marifleur/microservice/postgres/linux/util/net/NetUtil.java
@@ -1,10 +1,13 @@
 package pl.marifleur.microservice.postgres.linux.util.net;
 
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class NetUtil {
@@ -23,5 +26,20 @@ public class NetUtil {
             }
         }
         return null;
+    }
+
+    public String getMicroserviceBaseUrl(ServerProperties serverProperties) throws Exception {
+        if (Objects.nonNull(serverProperties)) {
+            String contextPath = serverProperties.getServlet().getContextPath();
+            String port = String.valueOf(serverProperties.getPort());
+
+            if (Objects.nonNull(contextPath)) {
+                return "http://" + getLocalHostIpAddress() + ":" + port + contextPath;
+            } else {
+                return "http://" + getLocalHostIpAddress() + ":" + port;
+            }
+        } else {
+            throw new RuntimeException("server properties is null");
+        }
     }
 }
